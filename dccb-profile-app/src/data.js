a139737c1c789cc44c6e5ae13f6f50258d0d3beb
Erick Rodriguez <erickrod54@gmail.com>
2022-05-27 22:08:10 -0400

    DCCB - Portfolio version 2 - data js - Features: Adding id prop to solve issue about key prop warning

--- dccb-profile-app/src/data.js
+++ dccb-profile-app/src/data.js
@@ -1,21 +1,25 @@
 /**
- * DCCB - Portfolio version 1 - data js - Features: 
+ * DCCB - Portfolio version 2 - data js - Features: 
  * 
- *          --> Creating the slice of data to the 'links'
+ *          --> Adding 'id' prop to solve issue
+ *              about key prop warning.
  * 
  * Note: there is more data for create here.
  */
 
 export const links = [
     {
+        id: 1,
         url:'/',
         text:'home'
     },
     {
+        id: 2,
         url:'/about-me',
         text:'about me'
     },
     {
+        id: 3,
         url:'/projects',
         text:'projects'
     }
