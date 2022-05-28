8c12df4274109a442d3b38f7b3c97dc4f314e16c
Erick Rodriguez <erickrod54@gmail.com>
2022-05-28 14:04:12 -0400

    DCCB - Portfolio version 3 - index js file - Features: Commenting index css file to start to clean css code, Importing and setting DccbGloabStyles

--- dccb-profile-app/src/index.js
+++ dccb-profile-app/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
+import DccbGlobalStyles from "./global.styles";
 import App from './App';
+
+/**DCCB - Portfolio version 3 - 'index' js file - 
+ * Features: 
+ *    
+ *          --> Commenting 'index.css' file
+ *              to start to clean css code.
+ * 
+ *          --> Importing and setting 
+ *              'DccbGlobalStyles'.
+ * 
+ * Note: By this point i can start to clean the code
+ * for the css sheet.
+ */
 
 ReactDOM.render(
   <React.StrictMode>
+    <DccbGlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
