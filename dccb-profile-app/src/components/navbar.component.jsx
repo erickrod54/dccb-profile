c1ddaf715c191e5fef0872bfeed5f1cfd0d4c81b
Erick Rodriguez <erickrod54@gmail.com>
2022-05-28 13:59:29 -0400

    DCCB - Portfolio version 3 - NavBar Component - Features: Building state to handle the theme, Building useEffect to set theme using the documentElement object, Building toggleTheme feature to trigger the switch between dark-theme and light-theme

--- dccb-profile-app/src/components/navbar.component.jsx
+++ dccb-profile-app/src/components/navbar.component.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { links } from "../data";
 import { NavLinks } from "../styled-components";
 import { NavLink } from "react-router-dom";
+
 /**
- * DCCB - Portfolio version 2 - NavBar Component- Features: 
+ * DCCB - Portfolio version 3 - NavBar Component- Features: 
  * 
- *          --> Fixing 'key' prop warning.
+ *          --> Building state to handle 'theme'.
+ * 
+ *          --> Building useEffect to set 'theme'   
+ *              using the 'documentElement' object.
+ * 
+ *          --> Building 'toggleTheme' feature to
+ *              trigger the switch between 'dark-theme'
+ *              and 'light-theme'
  * 
  * Note 'key' prop warning refers to the existence of an 'id'
  * for each child -this is applied to handle mapping a list
@@ -16,6 +24,21 @@ import { NavLink } from "react-router-dom";
  */
 
 const NavBar = () => {
+    /**here i build the state for the theme */
+    const [ theme, setTheme ] = useState('light-theme');
+
+    /**here i build the 'useEffect' to set theme*/
+    useEffect(() => {
+        document.documentElement.className = theme
+    }, [theme])
+
+    const toggleTheme = () => {
+        if (theme === 'dark-theme') {
+            setTheme('light-theme')
+        }else{
+            setTheme('dark-theme')
+        }
+    }
 
     return(
         <>
@@ -33,6 +56,13 @@ const NavBar = () => {
                     </ul>    
                 )
             })}
+            {/**here i build the button to switch betwwen 
+             * one theme and another*/}
+            <button 
+                className="btn btn-toggle" 
+                onClick={toggleTheme}>
+                toogle
+            </button>
         </NavLinks>
         </>     
     )
