197a7543fe9f0f8cf06e306350a930d73821fce8
Erick Rodriguez <erickrod54@gmail.com>
2022-05-31 09:45:43 -0400

    DCCB - Portfolio version 4 - Projects Page - Features: Mapping SingleProject Component, Spreading project and setting id for every SingleProject, Wrapping the Projects with a projects class Style

--- dccb-profile-app/src/pages/projects-page.jsx
+++ dccb-profile-app/src/pages/projects-page.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import SingleProject from "../components/single-project.component";
+import { projects } from '../data'
 /**
- * DCCB - Portfolio version 1 - Projects Page - Features: 
+ * DCCB - Portfolio version 4 - Projects Page - Features: 
  * 
- *          --> Implementing basic component for 'Projects'
- *              page.
+ *          --> Mapping 'SingleProject' Component.
  * 
- * Note:This component will have single project
+ *          --> Spreading 'project' and setting 'id'
+ *              for every 'SingleProject'
+ * 
+ *          --> Wrapping the 'Projects' with a 
+ *             'projects' class Style.
+ * 
+ * Note: this component is getting render in 'home-page'
+ * so will display the projects in the main page.
  */
 const Projects = () => {
 
     return(
         <>
             <h2>Projects Page</h2>
+            <article className='projects'>
+
+              {projects.map((project) => {
+                  console.log(project)
+                  return <SingleProject 
+                  {...project} key={project.id}/>
+              })}      
+
+            </article>
         </>
     )
 }
