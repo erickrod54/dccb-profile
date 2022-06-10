7a67fa72c17dd9f183be9f303ecc9f426adbfcc3
Erick Rodriguez <erickrod54@gmail.com>
2022-06-10 07:09:28 -0400

    DCCB - Portfolio version 5 - SingleProject Component - Features: Destructuring projects data props, Wrapping the SingleProject with a ProjectWrapper Style Component, Placing Pricing button -pending build functionality-

--- dccb-profile-app/src/components/single-project.component.jsx
+++ dccb-profile-app/src/components/single-project.component.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import { ProjectWrapper } from '../styled-components'
 /**
- * DCCB - Portfolio version 1 - SingleProject Component- Features: 
+ * DCCB - Portfolio version 5 - SingleProject Component- 
+ * Features: 
  * 
- *          --> Building Basic Component for SingleProject
+ *     --> Destructuring 'projects' data props.
  * 
- * Note:Here i can handle all the routting, and other features
- * as sharedlayout, there is an issue pending to solve with it
+ *     --> Wrapping the 'SingleProject' with a 
+ *         'ProjectWrapper' Style Component.
+ *     
+ *     --> Placing 'Pricing' button -pending
+ *         functionaility-
+ * 
+ * Note: this data was previously imported and mapped
+ * by 'Projects' Component on 'projects-page.jsx'. 
+ * 
+ * Pricing button is pending by functionality, is
+ * placed in this version for style porpouses.
  */
-const SingleProject = () => {
+
+const SingleProject = ({ title, owner, location }) => {
 
     return(
-        <>
-            <h2>SingleProject Component</h2>
-        </>
+     <ProjectWrapper>
+         <h3>{title}</h3>
+         <h4>owner: {owner}</h4>
+         <p><span> location: </span> {location}</p>
+         <button className="btn-primary">pricing</button>
+     </ProjectWrapper>      
     )
 }
 
