a3d70e74ad2f47c20e433d84bb361eca5a1598ff
Erick Rodriguez <erickrod54@gmail.com>
2022-06-21 00:16:14 -0400

    DCCB - Portfolio version 5 - data js - Features: Building projects test data - this is a initial phase of projects data

--- dccb-profile-app/src/data.js
+++ dccb-profile-app/src/data.js
@@ -1,10 +1,11 @@
 /**
- * DCCB - Portfolio version 2 - data js - Features: 
+ * DCCB - Portfolio version 5 - data js - Features: 
  * 
- *          --> Adding 'id' prop to solve issue
- *              about key prop warning.
+ *          --> Building 'projects' test data.
  * 
- * Note: there is more data for create here.
+ * Note: 'projects' test data is a sample data, pending
+ * shape a complete data.
+ * 
  */
 
 export const links = [
@@ -22,5 +23,89 @@ export const links = [
         id: 3,
         url:'/projects',
         text:'projects'
+    }
+]
+
+/**test data for projects */
+export const projects = [
+    {
+        id:1,
+        title:'luna dorada',
+        owner:'dilcia cardenas',
+        location:'cruz roja',
+        areaBuilt:255,
+        buildingType:'casa familiar'
+    },
+    {
+        id:2,
+        title:'galpones mack',
+        owner:'henry parra',
+        location:'avenida rotaria',
+        areaBuilt:255,
+        buildingType:'galpones'
+    },
+    {
+        id:3,
+        title:'Locales Comerciales Terminal de pasajeros',
+        owner:'Familia Pena',
+        location:'Terminal de Pasajeros',
+        areaBuilt:255,
+        buildingType:'Locales Comerciales'
+    },
+    {
+        id:4,
+        title:'las palmas',
+        owner:'manuel zambrano',
+        location:'las palmas',
+        areaBuilt:255,
+        buildingType:'casa bi familiar'
+    },
+    {
+        id:5,
+        title:'clinica santa cruz',
+        owner:'maite delgado',
+        location:'barrio obrero',
+        areaBuilt:255,
+        buildingType:'salud'
+    },
+    {
+        id:6,
+        title:'edificios santo tomas de aquino',
+        owner:'juan torres',
+        location:'santa teresa',
+        areaBuilt:255,
+        buildingType:'residencial'
+    },
+    {
+        id:7,
+        title:'funeraria los olivos',
+        owner:'fatima zambrano',
+        location:'gimnasio cubierto',
+        areaBuilt:255,
+        buildingType:'servicios funerarios'
+    },
+    {
+        id:8,
+        title:'centro comercial la trinidad',
+        owner:'oscar parra',
+        location:'los sauces',
+        areaBuilt:255,
+        buildingType:'comercial'
+    },
+    {
+        id:9,
+        title:'planta ozono',
+        owner:'oscar parra',
+        location:'centro',
+        areaBuilt:255,
+        buildingType:'industrial'
+    },
+    {
+        id:10,
+        title:'panaderia carabobo',
+        owner:'jose electricista',
+        location:'gran avenida',
+        areaBuilt:255,
+        buildingType:'comercial'
     }
 ]
