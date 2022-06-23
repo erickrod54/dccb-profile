8422f8fa428bd8236341152be63676be50a14241
Erick Rodriguez <erickrod54@gmail.com>
2022-06-23 00:10:26 -0400

    DCCB - Portfolio version 5 - Project Page - Features: Renaming file

---
+++ dccb-profile-app/src/pages/single.project-page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+/**DCCB - Portfolio version 5 - Projects Page - 
+ * Features: 
+ * 
+ *          --> Renaming file
+ * 
+ * Note: this page will display a single project page
+ * with details about the project previously hit by
+ * the user.
+ */
+const SingleProjectPage = () => {
+
+    return(
+    <article className='project'>
+        <h2>Single project</h2>
+    </article>
+    )
+}
+
+export default SingleProjectPage;
