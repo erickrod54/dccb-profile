f0c2498ce4a430f71e43654736f0c51019f2a5c7
Erick Rodriguez <erickrod54@gmail.com>
2022-07-19 10:48:02 -0400

    DCCB - Portfolio version 6 - Features: Adding temporal images and logo for design matters

---
+++ dccb-profile-app/src/assets/houses-images/index.js
@@ -0,0 +1,16 @@
+import image1 from '../houses-images/alejandra-cifre-gonzalez-ylyn5r4vxcA-unsplash.jpg';
+import image2 from '../houses-images/dillon-kydd-2keCPb73aQY-unsplash.jpg';
+import image3 from '../houses-images/dillon-kydd-pvdx8c6Y5BY-unsplash.jpg';
+import image4 from '../houses-images/krzysztof-hepner-V7Q0Oh3Az-c-unsplash.jpg';
+import image5 from '../houses-images/luke-stackpoole-eWqOgJ-lfiI-unsplash (1).jpg';
+import image6 from '../houses-images/r-architecture-JvQ0Q5IkeMM-unsplash.jpg';
+import image7 from '../houses-images/r-architecture-MAnVoJlQUvg-unsplash.jpg';
+import image8 from '../houses-images/terrah-holly-pmhdkgRCbtE-unsplash.jpg';
+import image9 from '../houses-images/todd-kent-178j8tJrNlc-unsplash.jpg';
+import image10 from '../houses-images/zac-gudakov-FH3zlDsY4Z4-unsplash.jpg';
+
+/**prisma images */
+import prismabcg1 from '../prisma-bcg.jpg';
+import prismabcg2 from '../prisma-bcg2.jpg';
+
+export {image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, prismabcg1, prismabcg2 }
