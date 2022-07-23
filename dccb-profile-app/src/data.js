7eea20c49746507b67ac7beb1d8e85c7b86336cd
Erick Rodriguez <erickrod54@gmail.com>
2022-07-23 00:15:15 -0400

    DCCB - Portfolio version 6 - data js - Features: Adding to links data design services link prop, Adding image and price props to projects test data

--- dccb-profile-app/src/data.js
+++ dccb-profile-app/src/data.js
@@ -1,11 +1,28 @@
+import { image1,
+         image2,
+         image3,
+         image4,
+         image5,
+         image6,
+         image7,
+         image8, 
+         image9,
+         image10 } from './assets/houses-images/index'
 /**
- * DCCB - Portfolio version 5 - data js - Features: 
+ * DCCB - Portfolio version 6 - data js - Features: 
  * 
- *          --> Building 'projects' test data.
+ *          --> Adding to 'links' data the 
+ *             'design services' link prop.
+ * 
+ *          --> Adding 'image' and 'price'
+ *              props to 'projects test data' 
  * 
  * Note: 'projects' test data is a sample data, pending
  * shape a complete data.
  * 
+ * By the momment the for 'projects' data i'll handle
+ * local images until i get done the definitive data
+ * shape
  */
 
 export const links = [
@@ -16,14 +33,19 @@ export const links = [
     },
     {
         id: 2,
-        url:'/about-me',
-        text:'about me'
+        url:'/projects',
+        text:'projects'
     },
     {
         id: 3,
-        url:'/projects',
-        text:'projects'
-    }
+        url:'/design-services',
+        text:'design services'
+    },
+    {
+        id: 4,
+        url:'/about-me',
+        text:'about me'
+    },
 ]
 
 /**test data for projects */
@@ -32,80 +54,101 @@ export const projects = [
         id:1,
         title:'luna dorada',
         owner:'dilcia cardenas',
+        image: image1,
+        price: 25999,
         location:'cruz roja',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'casa familiar'
     },
     {
         id:2,
         title:'galpones mack',
         owner:'henry parra',
+        image: image2,
+        price: 109999,
         location:'avenida rotaria',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'galpones'
     },
     {
         id:3,
         title:'Locales Comerciales Terminal de pasajeros',
         owner:'Familia Pena',
+        image: image3,
+        price: 309999,
         location:'Terminal de Pasajeros',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'Locales Comerciales'
     },
     {
         id:4,
         title:'las palmas',
         owner:'manuel zambrano',
+        image: image4,
+        price: 12599,
         location:'las palmas',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'casa bi familiar'
     },
     {
         id:5,
         title:'clinica santa cruz',
         owner:'maite delgado',
+        image: image5,
+        price: 4099,
         location:'barrio obrero',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'salud'
     },
     {
         id:6,
         title:'edificios santo tomas de aquino',
         owner:'juan torres',
+        image: image6,
+        price: 42999,
         location:'santa teresa',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'residencial'
     },
     {
         id:7,
         title:'funeraria los olivos',
         owner:'fatima zambrano',
+        image:image7,
+        price: 23999,
         location:'gimnasio cubierto',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'servicios funerarios'
     },
     {
         id:8,
         title:'centro comercial la trinidad',
         owner:'oscar parra',
+        image:image8,
+        price: 59999,
         location:'los sauces',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'comercial'
     },
     {
         id:9,
         title:'planta ozono',
         owner:'oscar parra',
+        image:image9,
+        price:39999,
         location:'centro',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'industrial'
     },
     {
         id:10,
         title:'panaderia carabobo',
         owner:'jose electricista',
+        image:image10,
+        price: 20099,
         location:'gran avenida',
-        areaBuilt:255,
+        builtarea:255,
         buildingType:'comercial'
     }
 ]
+
