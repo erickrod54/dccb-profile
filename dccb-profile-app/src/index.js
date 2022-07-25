fb2699b01fb8ae4c2f61f811ff99cf59a5658b21
Erick Rodriguez <erickrod54@gmail.com>
2022-07-25 01:00:58 -0400

    DCCB - Portfolio version 6 - index js file - Features: Wrapping with the AppProvider the whole application, Importing and placing PorjectsProvider to provide all concerns about projects

--- dccb-profile-app/src/index.js
+++ dccb-profile-app/src/index.js
@@ -3,24 +3,38 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import DccbGlobalStyles from "./global.styles";
 import App from './App';
+import { AppProvider } from '../src/context/dccb_profile_context'
+import { ProjectProvider } from '../src/context/projects_context'
 
-/**DCCB - Portfolio version 3 - 'index' js file - 
+/**DCCB - Portfolio version 6 - 'index' js file - 
  * Features: 
- *    
- *          --> Commenting 'index.css' file
- *              to start to clean css code.
  * 
- *          --> Importing and setting 
- *              'DccbGlobalStyles'.
+ *          --> Wrapping with the 'AppProvider' the
+ *              whole application.
  * 
- * Note: By this point i can start to clean the code
- * for the css sheet.
+ *          --> Importing and placing 'ProjectProvider'
+ *              to provide all concerns about projects.
+ * 
+ * Note: Version 6 start implementing context, and reducer to 
+ * handle much better the actions implementation.
+ * 
+ * By this point i can start to provide values from
+ * the dccb_profile_context > AppProvider to any Component
+ * that i need it
+ * 
+ * for this version i'm going to provide a test value as
+ * 'data' on 'Home' Component
  */
 
 ReactDOM.render(
   <React.StrictMode>
     <DccbGlobalStyles />
-    <App />
+    
+    <AppProvider>
+      <ProjectProvider>
+        <App />
+      </ProjectProvider>
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
