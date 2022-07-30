f1fcac3ff99e001eaf41252ba54f2970b01af578
Erick Rodriguez <erickrod54@gmail.com>
2022-07-29 23:04:49 -0400

    DCCB - Portfolio version 6 - NavBar Component - Features: Destructuring toggleTheme functionality from the context -testing by this version -, Placing a sign in / sign up message with a user icon to set the UI

---
+++ dccb-profile-app/src/components/navbar-buttons.component.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { useAppContext } from '../context/dccb_profile_context';
+
+import styled from "styled-components";
+
+//import { CgShoppingCart } from 'react-icons/cg'
+//import { BsToggleOn } from 'react-icons/bs'
+//import { BsToggleOff } from 'react-icons/bs'
+
+//To Login
+import { BsPersonCircle } from 'react-icons/bs'
+
+//Already login
+//import { FaUserCircle } from 'react-icons/fa'
+
+/**DCCB - Portfolio version 6 - NavBar Component- 
+ * Features: 
+ * 
+ *          --> Destructuring 'toggleTheme' 
+ *              functionality from the context.
+ *              - testing by this version-
+ * 
+ *          --> Placing a 'sign in / sigup' message
+ *              with a user icon to set the UI.
+ * 
+ * Note: This Component will keep the buttons
+ * for the navBar as login by the moment -later
+ * will be added more-
+ */
+
+const NavBarButtons = () => {
+
+    const { toggleTheme } = useAppContext()
+
+    console.log('toggle theme value ==> ', toggleTheme)
+
+    return(
+        <Wrapper className='cart-btn-wrapper'>
+          <div className="sign-in-up">
+            <h6>sign in / sign up</h6>
+          </div>
+            <button type="button" className="auth-btn">
+              <BsPersonCircle/> 
+            </button>
+            {/**<button type="button" onClick={toggleTheme}>
+                toggle
+            </button> */}
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  width: 225px;
+
+  .cart-btn {
+    color: var(--clr-grey-1);
+    font-size: 1.5rem;
+    letter-spacing: var(--spacing);
+    color: var(--clr-grey-1);
+    display: flex;
+
+    align-items: center;
+  }
+  .cart-container {
+    display: flex;
+    align-items: center;
+    position: relative;
+    svg {
+      height: 1.6rem;
+      margin-left: 5px;
+    }
+  }
+  .cart-value {
+    position: absolute;
+    top: -10px;
+    right: -16px;
+    background: var(--clr-primary-5);
+    width: 16px;
+    height: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-size: 0.75rem;
+    color: var(--clr-white);
+    padding: 12px;
+  }
+  .sign-in-up{
+    display: flex;
+    flex-direction: row;
+  }
+  .auth-btn {
+    display: flex;
+    align-items: center;
+    background: transparent;
+    border-color: transparent;
+    font-size: 1.50rem;
+    cursor: pointer;
+    color: var(--clr-grey-1);
+    letter-spacing: var(--spacing);
+    svg {
+      margin-left: 1px;
+    }
+    &:hover{
+      color: var(--clr-primary-16);
+    }
+  }
+`
+
+export default NavBarButtons;
