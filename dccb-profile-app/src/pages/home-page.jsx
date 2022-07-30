e3dc297973a85d335cc478455b9efe4196f54246
Erick Rodriguez <erickrod54@gmail.com>
2022-07-29 22:58:26 -0400

    DCCB - Portfolio version 6 - Home Page - Features: Importing and Placing Prisma, ProjectsPreview and ServicesComponent

--- dccb-profile-app/src/pages/home-page.jsx
+++ dccb-profile-app/src/pages/home-page.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import Prisma from '../components/prisma.component';
 import ProjectsPreview from "../components/projects-preview.component";
-/**
- * DCCB - Portfolio version 5 - Home Page - Features: 
+import ServicesComponent from "../components/services.component";
+
+/** DCCB - Portfolio version 6 - Home Page - Features: 
  * 
- *          --> Importing and Placing 'ProjectsPreview' 
- *              Component.
- * 
- *      
- * 
- * Note: This will render the 'Projects' Component on
- * 'HomePage' component.
- * 
+ *          --> Importing and Placing 'Prisma', 
+ *             'ProjectsPreview' and 
+ *             'ServicesComponent'
+ *     
+ * Note: By the momment this Components build the 'HomePage'
+ * but later in next versions is going to be added services 
+ * and team Components
  */
 
 const  HomePage = () => {
 
     return(
         <>
-            <h2>The Home Page</h2>
+            <Prisma />
             <ProjectsPreview />
+            <ServicesComponent />
         </>
     )
 }
