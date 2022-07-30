834e065f9418e0b48a73121cc8e80b0a18272c80
Erick Rodriguez <erickrod54@gmail.com>
2022-07-30 00:08:21 -0400

    DCCB - Portfolio version 6 - ProjectsPreview Component - Features: Building basic component for services

---
+++ dccb-profile-app/src/components/services.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**DCCB - Portfolio version 6 - 'ProjectsPreview' 
+ * Component- Features: 
+ * 
+ *     --> Building basic component
+ *        for services 
+ * 
+ * Note: This component in going to be use
+ * to build the specific services that later 
+ * will be added to the 'services-page'
+ */
+const ServicesComponent = () => {
+
+    return(
+        <h2>Services Component</h2>
+    )
+}
+
+export default ServicesComponent;
