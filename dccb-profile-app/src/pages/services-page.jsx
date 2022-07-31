63fa5ad9d9c321ddc088378fea510bef9e744ec8
Erick Rodriguez <erickrod54@gmail.com>
2022-07-31 12:25:34 -0400

    DCCB - Portfolio verion 6 - DesignServices - Features: Building DesignServices basic page - this page will be made with Component base on services-

---
+++ dccb-profile-app/src/pages/services-page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**DCCB - Portfolio version 6 - DesignServices 
+ * - Features: 
+ * 
+ *          --> Building 'DesignServices' basic
+ *              page.
+ * 
+ * Note: Here will be a detailed information about 
+ * the design services.
+ * 
+ */
+
+const DesignServices = () => {
+
+    return(
+        <h2>Design Services Component</h2>
+    )
+}
+
+export default DesignServices;
