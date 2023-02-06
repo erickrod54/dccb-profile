cb6251202ab6741944cb836f04c99cea52dffe29
Erick Rodriguez <erickrod54@gmail.com>
2023-02-05 23:21:05 -0500

    DCCB - Portfolio version 7.01 - Features: Importing and exporting Prisma and the rest of the Components, Changing versioning for two digits

---
+++ src/components/index.components.js
@@ -0,0 +1,18 @@
+import NavBar from "./navbar.component";
+import Prisma from "./prisma.component";
+import Footer from "./footer.component";
+import SideBar from "./sidebar.component";
+import ServicesComponent from "./services.component";
+
+/** DCCB - Portfolio version 7.01 - Features: 
+ * 
+ *      --> Importing and exporting Prisma.
+ * 
+ *      --> Changing versioning to two digits
+ * 
+ * Note: This index is built in order to rearrange 
+ * the components
+ */
+
+
+export { NavBar, Prisma, Footer, SideBar, ServicesComponent }
