3e79060b96dc7a0afcc31bcb09863e83d8f1b5a0
Erick Rodriguez <erickrod54@gmail.com>
2023-02-06 11:23:29 -0500

    DCCB - Portfolio version 7.01 - Prisma Component - Features: Wrapping the Component in a prisma-section, Changing versioning for two digits

--- src/components/prisma.component.jsx
+++ src/components/prisma.component.jsx
@@ -1,54 +1,58 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
-import { prismabcg1, prismabcg2 } from '../assets/houses-images/index' 
+import { useAppContext } from "../context/dccb_profile_context";
 
-/**
- * DCCB - Portfolio version 6 - Prisma Component- Features: 
+/** DCCB - Portfolio version 7.02 - Prisma Component- Features: 
  * 
- *          --> Building Prisma Component.
+ *      --> Wrapping the component in a 'prisma-section'.
  * 
- *          --> Importing and Placing two 
- *              presentation images 'prismabcg1'
- *              and ' prismabcg2'.
+ *      --> Changing versioning to two digits
  * 
- *          --> Building a 'Link' Component with
- *              btn btn-hero style to redirect to 
- *              the store.
- * 
- *          --> Setting a Presentation message.
- * 
- * Note:This Component will have presentation features for the
- * portfolio.
- * 
- * By the momment the 'NavContainer' Style Component 
- * will be set in each component, when the UI design 
- * get done they will renamed and live at 
- * 'styled-components.js'
+ * Note: fter this version i will use the 'id' for the 
+ * new layout.
  */
 
 const Prisma = () => {
 
+  const { arqpictures } = useAppContext()
+
+  console.log('here i got the arq pitures array ==>',arqpictures)
+
+  const arqroses = arqpictures[0].image;
+  const awards = arqpictures[1].image; 
+
     return(
+      <section id='prisma-section'>
         <Wrapper className='section-center'>
           <article className='content'>
-            <h1>design your <br />
-              space with proffesional ends
-            </h1>
-            <p>
-            augue lacus viverra vitae congue eu consequat
-            ac felis donec et odio pellentesque diam volutpat 
-            commodo sed egestas egestas fringilla
-            </p>
-              <Link to='/projects' className='btn hero-btn'>
-                shop now
-              </Link>
+          <h2>Greetings everyone</h2>
+              <p>
+                Welcome to my website i am <strong>Dilcia C. Cardenas B.</strong>,
+                an accomplished <strong>architect with 25 years of experience in 
+                the industry.</strong> 
+              </p>
+
+              <p>
+                With a mastery of <strong>AutoCAD</strong> dating back to 1997 and
+                active to the present date, and proficiency in BIM 
+                design using <strong>Archicad</strong>
+              </p> 
+              
+              <p>
+                I see myself <strong>as a valuable asset to any drafting team</strong>. 
+                <strong>My goal</strong> is <strong>to join a prestigious design company</strong> in 
+                the <strong>United States</strong> and <strong>bring her wealth of knowledge and 
+                skill to contribute to the success of new projects</strong>. 
+                Take a look around and <strong>discover the portfolio of this talented 
+                architect and learn why she is the right fit for your next project</strong>.
+              </p>
           </article>
-              <article className='img-container'>
-              <img src={prismabcg1} alt='nice table' className='main-img'/>
-              <img src={prismabcg2} alt='person working' className='accent-img'/>
-              </article>
+          <article className='img-container'>
+              <img src={arqroses} alt='nice table' className='main-img'/>
+              <img src={awards} alt='person working' className='accent-img'/>
+          </article>
         </Wrapper>
+      </section>
       )
 
 }
@@ -88,7 +92,7 @@ const Wrapper = styled.section`
     }
     .main-img {
       width: 100%;
-      height: 550px;
+      height:600px;
       position: relative;
       border-radius: var(--radius);
       display: block;
@@ -96,9 +100,9 @@ const Wrapper = styled.section`
     }
     .accent-img {
       position: absolute;
-      bottom: 0;
+      bottom: 1;
       left: 0;
-      width: 250px;
+      width: 350px;
       transform: translateX(-50%);
       border-radius: var(--radius);
     }
