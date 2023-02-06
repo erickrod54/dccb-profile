77d1facfbd2e896325fa36d557264e35948d2dc1
Erick Rodriguez <erickrod54@gmail.com>
2023-02-05 23:17:25 -0500

    DCCB - Portfolio version 7.01 - Features: Building index for pages, Importing and exporting Pages Components, Changing versioning to two digits

---
+++ src/pages/index.pages.js
@@ -0,0 +1,21 @@
+import Projects from "./projects-page";
+import AboutMe from "./about.me-page";
+import Error from "./error-page";
+import HomePage from "./home-page";
+import DesignServices from "./services-page";
+import SingleProjectPage from './projects-page';
+
+/** DCCB - Portfolio version 7.01 - Projects Page - Features: 
+ * 
+ *   --> Building index for pages. 
+ *        
+ *   --> Importing and exporting Pages Components. 
+ * 
+ *   --> Changing versioning to two digits
+ * 
+ * Note:This index is built in order to re arrange the components
+ * in the home page. 
+ * 
+ */
+
+export { AboutMe, Error, HomePage, Projects, DesignServices, SingleProjectPage }
