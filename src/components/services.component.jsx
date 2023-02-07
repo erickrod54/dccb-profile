6e0e79194a3ad8c0e16f0f0a86971c9101142f89
Erick Rodriguez <erickrod54@gmail.com>
2023-02-07 13:22:35 -0500

    DCCB - Portfolio version 7.02 - ServicesComponent - Features: Destructuring brands from the context, Placing icons and changing title

--- src/components/services.component.jsx
+++ src/components/services.component.jsx
@@ -1,21 +1,30 @@
 import React from "react";
+import { useAppContext } from "../context/dccb_profile_context";
 
 /**DCCB - Portfolio version 7.02 - 'ServicesComponent' 
  * Component- Features: 
  * 
- *     --> Wrapping the component in a 
- *         'services-component-section'.
+ *     --> Destructuring 'brands' from the context
  * 
- *      --> Changing versioning to two digits
+ *     --> Placing icons and changing the title.
  * 
  * Note: fter this version i will use the 'id' for the 
  * new layout.
  */
 const ServicesComponent = () => {
 
+    const { brands } = useAppContext()
+
+    console.log('brands ==>', brands)   
+    
+    const autocad = brands[0].image;
+    const archicad = brands[1].image;
+
     return(
         <section id='services-component-section'>
-            <h2>Services Component</h2>
+            <h2>I Design in </h2>
+            <img src={autocad} alt='autocad'></img>
+            <img src={archicad} alt='archicad'></img>
         </section>
     )
 }
