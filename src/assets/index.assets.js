9c1148d11f29c74c4a5b2af1fb52ee6840cd16ad
Erick Rodriguez <erickrod54@gmail.com>
2023-02-09 12:54:31 -0500

    DCCB - Portfolio version 7.03 - Features - index assets  - Features: Importing and exporting resume_eng and resume_span

---
+++ src/assets/index.assets.js
@@ -0,0 +1,16 @@
+import arqrosespic from './arq-dilcia-pictures/arq-roses-pic.jpg'
+import awards from './arq-dilcia-pictures/awards-1.jpg'
+import autocad from './brands/icons8-autocad.svg'
+import archicad from './brands/icons8-archicad.svg'
+import resume_eng from './resumes-eng-spanish/RESUMEN CURRICULAR 2023.-english.pdf';
+import resume_span from './resumes-eng-spanish/RESUMEN CURRICULAR 2023.-spanish.pdf';
+
+/** DCCB - Portfolio version 7.03 - index assets js - Features: 
+ * 
+ *       --> Importing and exporting 'resume_eng', and 
+ *          'resume_span'.
+ * 
+ * Note: re arranging assets trought the index assets
+ */
+
+export { arqrosespic, awards, autocad, archicad, resume_eng, resume_span }
