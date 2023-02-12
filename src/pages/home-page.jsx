22f7bdec0bd8367f937f038480fd1a5b24e0096b
Erick Rodriguez <erickrod54@gmail.com>
2023-02-12 11:47:34 -0500

    DCCB - Portfolio version 7.04 - Home Page - Features: Removing Footer and NavBar to add them to DccbSharedLayoutPage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Awards, Footer, NavBar, Prisma, Resumes, ServicesComponent } from '../components/index.components';
+import { Awards, Prisma, Resumes, ServicesComponent } from '../components/index.components';
 import AboutMe from "./about.me-page";
 
-/** DCCB - Portfolio version 7.03 - Home Page - Features: 
+/** DCCB - Portfolio version 7.04 - Home Page - Features: 
  * 
- *          --> Importing and palcing 'Resumes' section
- *              and 'Awards'.  
+ *          --> Removing 'Footer' and 'NavBar' to add them
+ *              to 'DccbSharedLayoutPage'.  
  *     
  * Note: This re arrange is made in order to change the layout from
  * 'flexbox' to 'grid'
@@ -15,13 +15,13 @@ const  HomePage = () => {
 
     return(
         <section className="container">
-            <NavBar />
+            
             <Prisma />
             <AboutMe />                      
             <ServicesComponent />
             <Resumes />
             <Awards /> 
-            <Footer />
+            
         </section>
        
     )
