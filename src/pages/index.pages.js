e83f4b7c41f33c17805038ae898bfe82ab338749
Erick Rodriguez <erickrod54@gmail.com>
2023-02-12 11:50:01 -0500

    DCCB - Portfolio version 7.04 - index.pages - Features: Importing and exporting DccbSharedLayoutPage

--- src/pages/index.pages.js
+++ src/pages/index.pages.js
@@ -4,18 +4,16 @@ import Error from "./error-page";
 import HomePage from "./home-page";
 import DesignServices from "./services-page";
 import SingleProjectPage from './projects-page';
+import DccbSharedLayoutPage from "./shared-layout-dccb.page";
 
-/** DCCB - Portfolio version 7.01 - Projects Page - Features: 
- * 
- *   --> Building index for pages. 
+/** DCCB - Portfolio version 7.04 - index.pages - Features: 
  *        
- *   --> Importing and exporting Pages Components. 
- * 
- *   --> Changing versioning to two digits
+ *   --> Importing and exporting 'DccbSharedLayoutPage'. 
+ *
  * 
  * Note:This index is built in order to re arrange the components
  * in the home page. 
  * 
  */
 
-export { AboutMe, Error, HomePage, Projects, DesignServices, SingleProjectPage }
+export { AboutMe, Error, HomePage, Projects, DesignServices, SingleProjectPage, DccbSharedLayoutPage }
