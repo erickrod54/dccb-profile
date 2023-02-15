4cceb077be69babc2c7e4986d1fbaab959af61cb
Erick Rodriguez <erickrod54@gmail.com>
2023-02-15 02:11:42 -0500

    DCCB - Portfolio version 7.06 - Features: Importing and exporting SocialMedia

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -5,15 +5,15 @@ import SideBar from "./sidebar.component";
 import ServicesComponent from "./services.component";
 import Awards from "./awards.component";
 import Resumes from "./resumes.component";
+import SocialMedia from "./social.media.component";
 
-/** DCCB - Portfolio version 7.03 - Features: 
+/** DCCB - Portfolio version 7.06 - Features: 
  * 
- *      --> Importing and exporting 'Resumes' 
- *          and 'Awards'.
+ *      --> Importing and exporting 'SocialMedia'.
  * 
  * Note: This index is built in order to rearrange 
  * the components
  */
 
 
-export { NavBar, Prisma, Footer, SideBar, ServicesComponent, Awards, Resumes }
+export { NavBar, Prisma, Footer, SideBar, ServicesComponent, Awards, Resumes, SocialMedia }
