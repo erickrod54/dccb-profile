a0708019c08785fc56cfb21011370a944c1e4c9e
Erick Rodriguez <erickrod54@gmail.com>
2023-02-17 11:42:45 -0500

    DCCB - Portfolio version 7.07 - PrismaContent - Features: Building PrismaContent

---
+++ src/components/prisma.content.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+/** DCCB - Portfolio version 7.07 - PrismaContent - Features: 
+ * 
+ *      --> Building 'PrismaContent'
+ * 
+ * Note: This index is built in order to rearrange 
+ * the components
+ */
+
+const PrismaContent = () => {
+
+    return(
+        <article className='content'>
+          <h2>Greetings everyone</h2>
+              <p>
+                Welcome to my website i am <strong>Dilcia C. Cardenas B.</strong>,
+                an accomplished <strong>architect with 25 years of experience in 
+                the industry.</strong> 
+              </p>
+
+              <p>
+                With a mastery of <strong>AutoCAD</strong> dating back to 1997 and
+                active to the present date, and proficiency in BIM 
+                design using <strong>Archicad</strong>
+              </p> 
+              
+              <p>
+                I see myself <strong>as a valuable asset to any drafting team </strong>. 
+                and <strong>My goal</strong> is <strong> to join a prestigious design company</strong> in 
+                the <strong>United States</strong> and <strong>bring her wealth of knowledge and 
+                skill to contribute to the success of new projects</strong>. 
+                Take a look around and <strong>discover the portfolio of this talented 
+                architect and learn why she is the right fit for your next project</strong>.
+              </p>
+          </article>
+    )
+}
+
+export default PrismaContent;
