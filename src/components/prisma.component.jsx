fa99816df35253c8bf23a8938a8d8dc741f505c9
Erick Rodriguez <erickrod54@gmail.com>
2023-02-17 11:39:29 -0500

    DCCB - Portfolio version 7.07 - Prisma Component- Features: Implementing Prisma in two separate components

--- src/components/prisma.component.jsx
+++ src/components/prisma.component.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { useAppContext } from "../context/dccb_profile_context";
+import { PrismaContent, PrismaImages } from './index.components'
 
-/** DCCB - Portfolio version 7.02 - Prisma Component- Features: 
+/** DCCB - Portfolio version 7.07 - Prisma Component- Features: 
  * 
- *      --> Destructuring 'arqpictures' from the context.
- * 
- *      --> Writing an introduction message.
- * 
- *      --> Adding 'img-container-section' to set grid styles.
- * 
- *      --> clearing media queries.
+ *      --> Implemenmting Prisma in two separate components.
  * 
  * Note: fter this version i will use the 'id' for the 
  * new layout.
@@ -18,45 +12,11 @@ import { useAppContext } from "../context/dccb_profile_context";
 
 const Prisma = () => {
 
-  const { arqpictures } = useAppContext()
-
-  console.log('here i got the arq pitures array ==>',arqpictures)
-
-  const arqroses = arqpictures[0].image;
-  const awards = arqpictures[1].image; 
-
     return(
       <section id='prisma-section'>
         <Wrapper className='section-center'>
-          <article className='content'>
-          <h2>Greetings everyone</h2>
-              <p>
-                Welcome to my website i am <strong>Dilcia C. Cardenas B.</strong>,
-                an accomplished <strong>architect with 25 years of experience in 
-                the industry.</strong> 
-              </p>
-
-              <p>
-                With a mastery of <strong>AutoCAD</strong> dating back to 1997 and
-                active to the present date, and proficiency in BIM 
-                design using <strong>Archicad</strong>
-              </p> 
-              
-              <p>
-                I see myself <strong>as a valuable asset to any drafting team </strong>. 
-                and <strong>My goal</strong> is <strong> to join a prestigious design company</strong> in 
-                the <strong>United States</strong> and <strong>bring her wealth of knowledge and 
-                skill to contribute to the success of new projects</strong>. 
-                Take a look around and <strong>discover the portfolio of this talented 
-                architect and learn why she is the right fit for your next project</strong>.
-              </p>
-          </article>
-          <section id='img-container-section'>
-          <article className='img-container'>
-              <img src={arqroses} alt='nice table' className='main-img'/>
-              <img src={awards} alt='person working' className='accent-img'/>
-          </article>
-          </section>
+          <PrismaContent />
+          <PrismaImages />
         </Wrapper>
       </section>
       )
