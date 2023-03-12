d97aaaed659099984fd3ff777220ee1de7b4b0b5
Erick Rodriguez <erickrod54@gmail.com>
2023-03-12 16:37:54 -0400

    DCCB - Portfolio version 7.09 - PrismaComponent - Features: Refactoring name classes, Applying naming convention

--- src/components/prisma.content.component.jsx
+++ src/components/prisma.content.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-/** DCCB - Portfolio version 7.07 - PrismaContent - Features: 
+/** DCCB - Portfolio version 7.09 - PrismaContent - Features: 
  * 
- *      --> Building 'PrismaContent'
+ *      --> Refactoring style name classes.
+ * 
+ *      --> Applying style naming convention.
  * 
  * Note: This index is built in order to rearrange 
  * the components
@@ -11,29 +13,28 @@ import React from "react";
 const PrismaContent = () => {
 
     return(
-        <article className='content'>
-          <h2>Greetings everyone</h2>
-              <p>
+        <section id='intro'>
+          <h2 className="intro--title">Greetings everyone</h2>
+              <p className="intro--para">
+
                 Welcome to my website i am <strong>Dilcia C. Cardenas B.</strong>,
                 an accomplished <strong>architect with 25 years of experience in 
                 the industry.</strong> 
-              </p>
-
-              <p>
+              
                 With a mastery of <strong>AutoCAD</strong> dating back to 1997 and
                 active to the present date, and proficiency in BIM 
                 design using <strong>Archicad</strong>
-              </p> 
               
-              <p>
                 I see myself <strong>as a valuable asset to any drafting team </strong>. 
                 and <strong>My goal</strong> is <strong> to join a prestigious design company</strong> in 
                 the <strong>United States</strong> and <strong>bring her wealth of knowledge and 
                 skill to contribute to the success of new projects</strong>. 
                 Take a look around and <strong>discover the portfolio of this talented 
                 architect and learn why she is the right fit for your next project</strong>.
+              
               </p>
-          </article>
+
+          </section>
     )
 }
 
