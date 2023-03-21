0fa771da4d5e197fbbc4f60cf6f1c4c12c213643
Erick Rodriguez <erickrod54@gmail.com>
2023-03-21 01:15:15 -0400

    DCCB - Portfolio version  7.11 - Prisma Component - Features: Refactoring title, Modifying reference to BIM Archicad, Adding Important Message about english speaker

--- src/components/prisma.content.component.jsx
+++ src/components/prisma.content.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-/** DCCB - Portfolio version 7.09 - PrismaContent - Features: 
+/** DCCB - Portfolio version 7.11 - PrismaContent - Features: 
  * 
- *      --> Refactoring style name classes.
+ *      --> Refactoring title.
  * 
- *      --> Applying style naming convention.
+ *      --> Modififying reference to BIM Archicad.
+ * 
+ *      --> Adding 'Important Message' about english
+ *          speaker.
  * 
  * Note: This index is built in order to rearrange 
  * the components
@@ -14,7 +17,7 @@ const PrismaContent = () => {
 
     return(
         <section id='intro'>
-          <h2 className="intro--title">Greetings everyone</h2>
+          <h2 className="intro--title">I'm Dilcia! Welcome to my Website</h2>
               <p className="intro--para">
 
                 Welcome to my website i am <strong>Dilcia C. Cardenas B.</strong>,
@@ -22,8 +25,7 @@ const PrismaContent = () => {
                 the industry.</strong> 
               
                 With a mastery of <strong>AutoCAD</strong> dating back to 1997 and
-                active to the present date, and proficiency in BIM 
-                design using <strong>Archicad</strong>
+                active to the present date
               
                 I see myself <strong>as a valuable asset to any drafting team </strong>. 
                 and <strong>My goal</strong> is <strong> to join a prestigious design company</strong> in 
@@ -32,6 +34,10 @@ const PrismaContent = () => {
                 Take a look around and <strong>discover the portfolio of this talented 
                 architect and learn why she is the right fit for your next project</strong>.
               
+              </p>
+
+              <p className="intro--important"><span className="intro--important-color">Important:</span> I do not speak english, but i have no problem to write and take
+                projects via text, email or any other written media.
               </p>
 
           </section>
