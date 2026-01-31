46ee1af25644d79bba1d45bdeb245f7c7aafc782
Erick Rodriguez <erickrod54@gmail.com>
2026-01-30 21:31:00 -0600

    footer component in legacy repo

---
+++ legacy-repo/components-legacy/footer.component.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+/**
+ * DCCB - Portfolio version 7.02 - Footer Component- Features: 
+ * 
+ *          --> Wrapping the component in a 'footer-section'.
+ * 
+ *          --> Changing versioning to two digits
+ * 
+ * Note: fter this version i will use the 'id' for the 
+ * new layout.
+ */
+
+const Footer = () => {
+
+    return(
+      <section id='footer-section'>
+        <Wrapper>
+            <h5>&copy; { new Date().getFullYear()}</h5>
+            <span> DCCB  |  Arquitecture and Design </span>
+            <h5>  All Rights reserved</h5>
+        </Wrapper>
+      </section>
+    )
+}
+
+const Wrapper = styled.footer`
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--clr-black);
+  text-align: center;
+  span {
+    color: var(--clr-primary-13);
+  }
+  h5 {
+    color: var(--clr-white);
+    margin: 0.5rem;
+
+    font-weight: 400;
+    text-transform: none;
+    line-height: 1.25;
+  }
+  @media (min-width: 776px) {
+    flex-direction: row;
+  }
+`
+
+export default Footer;
