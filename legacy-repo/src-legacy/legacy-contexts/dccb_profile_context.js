7aa4552e441b894c16d2dac2625ca3bfc3ecc7fb
Erick Rodriguez <erickrod54@gmail.com>
2026-02-04 21:32:00 -0600

    context dccb_profile_context in legacy context directory

---
+++ legacy-repo/src-legacy/legacy-contexts/dccb_profile_context.js
@@ -0,0 +1,88 @@
+import React, { useContext, useState, useEffect, useReducer } from "react";
+import reducer from '../reducers/dccb_profile_reducers';
+import { arqpictures, brands, links, publicAwards, SocialIcons } from '../data'
+import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../utils/actions";
+
+/**DCCB - Portfolio version 7.09 - 'dccb_profile_context' file - 
+ * Features: 
+ * 
+ *          --> Providing 'setShowresumes' to set useEffect on
+ *              resumes.component.
+ * 
+ * Note: pending to modify context name
+ * */
+
+/**here i build the initial state*/
+const initialState = {
+    /**initial state for 'Sidebar' */
+    isSidebarOpen: true,
+}
+
+
+export const AppContext = React.createContext()
+
+export const AppProvider = ({ children }) => {
+    const [ state, dispatch ] = useReducer( reducer, initialState);
+    
+    /**states for the resumes */
+    const [ showresumes, setShowresumes ] = useState(false);
+
+    const openSidebar = () => {
+        dispatch({ type: SIDEBAR_OPEN})
+    }
+
+    const closeSidebar = () => {
+        dispatch({ type: SIDEBAR_CLOSE})
+    }
+
+    /**pending to dispatch and build an action to handle 
+     * the toggle theme*/
+    
+    /**here i build the state for the theme */
+    const [ theme, setTheme ] = useState('light-theme');
+
+    /**here i build the 'useEffect' to set theme*/
+    useEffect(() => {
+        document.documentElement.className = theme
+    }, [theme])
+
+    const toggleTheme = () => {
+        if (theme === 'dark-theme') {
+            setTheme('light-theme')
+        }else{
+            setTheme('dark-theme')
+        }
+    }
+    
+     /**handlers for the 'handleShowresumes' */
+    const handleShowresumes = () => {
+        setShowresumes(!showresumes)
+    }
+
+    return(
+        <AppContext.Provider 
+            value={{
+                ...state, 
+                links,
+                theme,
+                showresumes,
+                toggleTheme,
+                openSidebar,
+                closeSidebar,
+                arqpictures,
+                brands,
+                publicAwards,
+                handleShowresumes,
+                SocialIcons,
+                setShowresumes
+                }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
+
+
