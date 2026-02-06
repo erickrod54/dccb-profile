521abe49a3f70c9d750db7f1ac4a9bfa0a0bbd85
Erick Rodriguez <erickrod54@gmail.com>
2026-02-05 21:30:00 -0600

    App js in src legacy repo

---
+++ legacy-repo/src-legacy/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router,
+         Routes, 
+         Route } from 'react-router-dom';
+
+import { HomePage, AboutMe, Projects, SingleProjectPage, DesignServices, Error, DccbSharedLayoutPage } from './pages/index.pages';
+
+/** DCCB - Portfolio version 7.10 - Features: 
+ * 
+ *      --> Changing '/home' to '/'.
+ * 
+ * Note: This is made in order the get the 
+ * 'Navbar' and 'Footer' present over the
+ * navigation. 
+ */
+
+function App() {
+  return (
+  
+      <Router>
+        <Routes>
+            <Route path='/' element={<DccbSharedLayoutPage />}>
+              <Route index path='/' element={<HomePage />}/> 
+              <Route path='/about-me' element={<AboutMe />}/>
+              <Route path='/projects' element={<Projects />}/>
+              <Route path='/projects/:id' element={<SingleProjectPage />}/>
+              <Route path='/design-services' element={<DesignServices />}/>
+              <Route path='*' element={<Error />}/>
+            </Route>
+        </Routes>
+      </Router>
+    
+  );
+}
+
+export default App;
