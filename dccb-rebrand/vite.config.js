aca9e381a9aa9e3a479bd0879323fee92c7a8f7f
Erick Rodriguez <erickrod54@gmail.com>
2026-02-13 21:35:00 -0600

    adding host true to expose the app to the network (so i can see work from any of my devices)

--- dccb-rebrand/vite.config.js
+++ dccb-rebrand/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,    // This exposes the server to your local network
+    port: 5173,
+    strictPort: true,
+  }
 })
