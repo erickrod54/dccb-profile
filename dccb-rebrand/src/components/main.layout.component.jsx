5656f9e64ebfda1abda7e8ca5b813183ca324542
Erick Rodriguez <erickrod54@gmail.com>
2026-02-14 21:30:00 -0600

    DCCB-rebrand - Portfolio version 1.00 - MainLayout - Features: Building MainLayout component

---
+++ dccb-rebrand/src/components/main.layout.component.jsx
@@ -0,0 +1,16 @@
+/** DCCB - Portfolio version 1.00 - MainLayout - Features: 
+ * 
+ *      --> Building MainLayout component
+ * 
+ * Note: This component will be the foundation for the
+ * new layout for dccb re brand.
+ */
+
+
+const MainLayout = () => {
+    return (
+        <h2>Main layout for DCCB re brand</h2>
+    )
+}
+
+export default MainLayout;
