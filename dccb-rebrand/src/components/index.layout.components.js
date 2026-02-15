bdadf0acf710a7a278e1943ebcc929f2c9fcd14c
Erick Rodriguez <erickrod54@gmail.com>
2026-02-14 21:32:00 -0600

    DCCB-rebrand - Portfolio version 1.00 - layout.index - Features: Building layout index

---
+++ dccb-rebrand/src/components/index.layout.components.js
@@ -0,0 +1,11 @@
+import MainLayout from "./main.layout.component.jsx";
+
+/** DCCB-rebrand - Portfolio version 1.00 - layout.index 
+ * - Features: 
+ * 
+ *      --> Building layout index.
+ * 
+ * Note: This is the index of the main layout. 
+ */
+
+export { MainLayout }
