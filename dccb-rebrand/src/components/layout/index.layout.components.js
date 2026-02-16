ba6c574c06845e925c167c746347ba111574b12d
Erick Rodriguez <erickrod54@gmail.com>
2026-02-15 21:32:00 -0600

    DCCB-rebrand - Portfolio version 1.03 - layoout.index - Features: Changing MainLayout path

---
+++ dccb-rebrand/src/components/layout/index.layout.components.js
@@ -0,0 +1,11 @@
+import MainLayout from "./main.layout.component.jsx";
+
+/** DCCB-rebrand - Portfolio version 1.02 - layout.index 
+ * - Features: 
+ * 
+ *      --> Changing MainLayout path.
+ * 
+ * Note: This is the index of the main layout. 
+ */
+
+export { MainLayout }
