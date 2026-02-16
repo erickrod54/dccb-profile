6bf687500ee24bbf65e47fd1c293c669be9f603f
Erick Rodriguez <erickrod54@gmail.com>
2026-02-15 21:30:00 -0600

    DCCB-rebrand - Portfolio version 1.01 - MainLayout - Features: Moving MainLayout component to layout directory

---
+++ dccb-rebrand/src/components/layout/main.layout.component.jsx
@@ -0,0 +1,16 @@
+/** DCCB-rebrand - Portfolio version 1.01 - MainLayout - Features: 
+ * 
+ *      --> Moving MainLayout component to layout directory 
+ * 
+ * Note: This component will be the foundation for the
+ * new layout for dccb re brand.
+ */
+
+
+const MainLayout = () => {
+    return (
+        <h2>Main layout for DCCB re brand</h2>
+    )
+}
+
+export default MainLayout;
