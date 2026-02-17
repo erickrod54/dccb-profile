850101d40f0e0146e9ef4f721a624c1fee22e27a
Erick Rodriguez <erickrod54@gmail.com>
2026-02-16 21:32:00 -0600

    DCCB-rebrand - Portfolio version 1.03 - vite.config - Features: Adding tailwind to vite config file

--- dccb-rebrand/vite.config.js
+++ dccb-rebrand/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+/** DCCB-rebrand - Portfolio version 1.03 - vite.config - 
+ * Features: 
+ * 
+ *      --> Adding 'tailwind'.
+ * 
+ * Note: This is the first component in the re brand
+ */
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     host: true,    // This exposes the server to your local network
     port: 5173,
