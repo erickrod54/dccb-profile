6a9f311ad2ae6b1657ccb49e4fb9f2afad920320
Erick Rodriguez <erickrod54@gmail.com>
2026-02-17 21:30:00 -0600

    DCCB-rebrand - Portfolio version 1.04 - vite.config - Features: Setting and enabling basicSsl

--- dccb-rebrand/vite.config.js
+++ dccb-rebrand/vite.config.js
@@ -1,24 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-/** DCCB-rebrand - Portfolio version 1.03 - vite.config - 
+/** DCCB-rebrand - Portfolio version 1.04 - vite.config - 
  * Features: 
  * 
- *      --> Adding 'tailwind'.
+ *      --> Setting and enabling 'basicSsl'.
  * 
- * Note: This is the first component in the re brand
+ * Note: 'basicSsl' is for https protocol self signed certificate
  */
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    basicSsl()//this generates the temporary self signed certificate
   ],
   server: {
     host: true,    // This exposes the server to your local network
     port: 5173,
     strictPort: true,
+    https:true //Enables TLS/SSL protocol
   }
 })
