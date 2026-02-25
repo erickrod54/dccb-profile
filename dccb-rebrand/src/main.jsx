af6609ab3d126bc848fec0d7c762c04ba991cd99
Erick Rodriguez <erickrod54@gmail.com>
2026-02-24 21:33:00 -0600

    DCCB-rebrand - Portfolio version 1.05 - Features: Wrapping using ThemeProvider

--- dccb-rebrand/src/main.jsx
+++ dccb-rebrand/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/theme.context'
+
+/** DCCB-rebrand - Portfolio version 1.05 - Features: 
+ * 
+ *      --> Wrapping using 'ThemeProvider'.
+ * 
+ * Note: 'TailwindTest' is a component to test 
+ * tailwind 
+ */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
