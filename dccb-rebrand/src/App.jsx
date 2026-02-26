3fdb0c7203ce4d7b3bbf306cf2344d12d27ca974
Erick Rodriguez <erickrod54@gmail.com>
2026-02-25 21:32:00 -0600

    DCCB-rebrand - Portfolio version 1.06 - Features: Adding Hero Component to MainLayout

--- dccb-rebrand/src/App.jsx
+++ dccb-rebrand/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 import { MainLayout } from './components/layout/index.layout.components'
+import Hero from './components/sections/hero.section.component'
 
-/** DCCB-rebrand - Portfolio version 1.05 - Features: 
+/** DCCB-rebrand - Portfolio version 1.06 - Features: 
  * 
- *      --> Removing Testing components.
+ *      --> Adding Hero Component to MainLayout.
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
@@ -13,7 +14,9 @@ function App() {
 
   return (
     <>
-      <MainLayout />
+      <MainLayout >
+        <Hero />
+      </MainLayout>
     </>
   )
 }
