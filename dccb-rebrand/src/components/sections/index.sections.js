bceb80729990a6079b85064e616926ec1a00ba28
Erick Rodriguez <erickrod54@gmail.com>
2026-02-25 21:31:00 -0600

    DCCB-rebrand - Portfolio version 1.05 - Features: Building Hero section Title

---
+++ dccb-rebrand/src/components/sections/index.sections.js
@@ -0,0 +1,11 @@
+import Hero from './hero.section.component'
+
+/** DCCB-rebrand - Portfolio version 1.05 - Features: 
+ * 
+ *      --> Importing and exporting Hero from index.
+ * 
+ * Note: 'TailwindTest' is a component to test 
+ * tailwind 
+ */
+
+export { Hero }
