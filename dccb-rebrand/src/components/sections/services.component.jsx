951aefa838000f324cb45a1ad019d89fb568f72b
Erick Rodriguez <erickrod54@gmail.com>
2026-02-26 21:34:00 -0600

    DCCB-rebrand - Portfolio version 1.13 - Features: Adding second service

--- dccb-rebrand/src/components/sections/services.component.jsx
+++ dccb-rebrand/src/components/sections/services.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Ruler, Map, Layers, ClipboardCheck, Zap, Icon } from 'lucide-react'
 import { cn } from "@/lib/utils";
 
-/** DCCB-rebrand - Portfolio version 1.12 - Features: 
+/** DCCB-rebrand - Portfolio version 1.13 - Features: 
  * 
- *      --> Adding first service.
+ *      --> Adding second service.
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
@@ -17,5 +17,12 @@ const services = [
         Icon: <Ruler className="w-5 h-5" />,
         code: 'SEC_01',
         featured:false
+    },
+    {
+        title: "URBAN PLANING",
+        des: 'Analisis del sitio, esquemas de zonificacion y planificacion de desarrollo urbano sostenible',
+        icon: <Map className="w-5 h-5" />,
+        code: 'SEC_02',
+        featured: false
     }
 ]
