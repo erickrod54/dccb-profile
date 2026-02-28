15233fe8b3046e85dbbc61f6471f81cc518ecc68
Erick Rodriguez <erickrod54@gmail.com>
2026-02-27 21:33:00 -0600

    DCCB-rebrand - Portfolio version 1.18 - Features: Importing and exporting Services from index

--- dccb-rebrand/src/components/sections/index.sections.js
+++ dccb-rebrand/src/components/sections/index.sections.js
@@ -1,11 +1,12 @@
 import Hero from './hero.section.component'
+import Services from './services.component'
 
-/** DCCB-rebrand - Portfolio version 1.05 - Features: 
+/** DCCB-rebrand - Portfolio version 1.18 - Features: 
  * 
- *      --> Importing and exporting Hero from index.
+ *      --> Importing and exporting Services from index.
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
  */
 
-export { Hero }
+export { Hero, Services }
