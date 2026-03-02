c43cb747b7eebb1ac94f34be6091b4e78b2809bb
Erick Rodriguez <erickrod54@gmail.com>
2026-03-01 21:36:00 -0600

    DCCB-rebrand - Portfolio version 2.07 - Features: Importing and exporting BeforeAndAfterSlider

---
+++ dccb-rebrand/src/components/features/index.features.js
@@ -0,0 +1,11 @@
+import BeforeAfterSlider from "./before.after.slider.component";
+
+/** DCCB-rebrand - Portfolio version 2.07 - Features: 
+ * 
+ *      --> Importing and exporting 'BeforeAfterSlider'.
+ * 
+ * Note: 'TailwindTest' is a component to test 
+ * tailwind 
+ */
+
+export { BeforeAfterSlider }
