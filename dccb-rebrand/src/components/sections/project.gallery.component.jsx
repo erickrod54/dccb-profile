97e1bc97f3f49a79d319cab898a1da64c68ab4a1
Erick Rodriguez <erickrod54@gmail.com>
2026-03-02 21:31:00 -0600

    DCCB-rebrand - Portfolio version 2.07 - Features: Adding BeforeAfterSlider component

---
+++ dccb-rebrand/src/components/sections/project.gallery.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import BeforeAfterSlider from '../features/before.after.slider.component';
+
+/** DCCB-rebrand - Portfolio version 2.07 - Features: 
+ * 
+ *      --> Adding 'BeforeAfterSlider' component.
+ * 
+ * Note: 'TailwindTest' is a component to test 
+ * tailwind 
+ */
+
+
+const ProjectGallery = () => {
+
+    return (
+        <BeforeAfterSlider />
+    )
+}
+
+export default ProjectGallery;
