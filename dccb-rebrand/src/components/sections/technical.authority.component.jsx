bd18f6b54abfe23c5f44957ee12426925439e164
Erick Rodriguez <erickrod54@gmail.com>
2026-03-04 21:35:00 -0600

    DCCB-rebrand - Portfolio version 2.16 - Features: Building TechnicalAuthority section

---
+++ dccb-rebrand/src/components/sections/technical.authority.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ShieldCheck, HardHat, Ruler, Globe } from 'lucide-react';
+
+/** DCCB-rebrand - Portfolio version 2.16 - Features: 
+ * 
+ *      --> Building TechnicalAuthority section.
+ * 
+ * Note: 'TailwindTest' is a component to test 
+ * tailwind 
+ */
+
+const TechnicalAuthority = () => {
+
+    return (
+        <section className='py-20 border-t border-arch-gray/10 dark:border-white/5'>
+
+        </section>
+    )
+}
+
+export default TechnicalAuthority;
