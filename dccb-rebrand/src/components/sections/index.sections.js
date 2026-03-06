8844eec751353f41584ab4f5d17d1e468ab1a099
Erick Rodriguez <erickrod54@gmail.com>
2026-03-05 21:30:00 -0600

    DCCB-rebrand - Portfolio version 2.16 - Features: Importing and exporting TechnicalAuthority from index

--- dccb-rebrand/src/components/sections/index.sections.js
+++ dccb-rebrand/src/components/sections/index.sections.js
@@ -1,13 +1,14 @@
 import Hero from './hero.section.component'
 import ProjectGallery from './project.gallery.component'
 import Services from './services.component'
+import TechnicalAuthority from './technical.authority.component'
 
-/** DCCB-rebrand - Portfolio version 2.07 - Features: 
+/** DCCB-rebrand - Portfolio version 2.16 - Features: 
  * 
- *      --> Importing and exporting 'ProjectGallery' from index.
+ *      --> Importing and exporting 'TechnicalAuthority' from index.
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
  */
 
-export { Hero, Services, ProjectGallery }
+export { Hero, Services, ProjectGallery, TechnicalAuthority }
