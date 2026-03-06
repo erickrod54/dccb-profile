14f6f5ff53a2d1edd83603bc2a0cbbccfd8cdc08
Erick Rodriguez <erickrod54@gmail.com>
2026-03-05 21:31:00 -0600

    DCCB-rebrand - Portfolio version 2.17 - Features: Adding TecchnicalAuthority title

--- dccb-rebrand/src/components/sections/technical.authority.component.jsx
+++ dccb-rebrand/src/components/sections/technical.authority.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ShieldCheck, HardHat, Ruler, Globe } from 'lucide-react';
 
-/** DCCB-rebrand - Portfolio version 2.16 - Features: 
+/** DCCB-rebrand - Portfolio version 2.17 - Features: 
  * 
- *      --> Building TechnicalAuthority section.
+ *      --> Adding TechnicalAuthority title.
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
@@ -13,7 +13,12 @@ const TechnicalAuthority = () => {
 
     return (
         <section className='py-20 border-t border-arch-gray/10 dark:border-white/5'>
-
+            <div className='flex items-center gap-4 mb-16'>
+                <h2 className='font-mono text-xl font-bold tracking-tighter uppercase dark:text-white'>
+                    03 // CAREER_AUTHORITY
+                </h2>
+                <div className='h-[1px] flex-1 bg-arch-gray/10 dark:bg-white/5'></div>
+            </div>
         </section>
     )
 }
