07eb4c729c1b1751e2b787e6057cf792cea9d487
Erick Rodriguez <erickrod54@gmail.com>
2026-03-06 21:32:00 -0600

    DCCB-rebrand - Portfolio version 3.03 - Features: Importing and exporting AboutMeDCCB from index

--- dccb-rebrand/src/components/sections/index.sections.js
+++ dccb-rebrand/src/components/sections/index.sections.js
@@ -1,14 +1,15 @@
+import AboutMeDCCB from './about.me.component'
 import Hero from './hero.section.component'
 import ProjectGallery from './project.gallery.component'
 import Services from './services.component'
 import TechnicalAuthority from './technical.authority.component'
 
-/** DCCB-rebrand - Portfolio version 2.16 - Features: 
+/** DCCB-rebrand - Portfolio version 3.03 - Features: 
  * 
- *      --> Importing and exporting 'TechnicalAuthority' from index.
+ *      --> Importing and exporting 'AboutMeDCCB' from index.
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
  */
 
-export { Hero, Services, ProjectGallery, TechnicalAuthority }
+export { Hero, Services, ProjectGallery, TechnicalAuthority, AboutMeDCCB }
