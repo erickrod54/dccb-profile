2fda94020d4a3bd4594a48adc9bbd93b20d2ca98
Erick Rodriguez <erickrod54@gmail.com>
2026-03-06 21:35:00 -0600

    DCCB-rebrand - Portfolio version 3.04 - Features: Adding dccb_about_picture

--- dccb-rebrand/src/components/sections/about.me.component.jsx
+++ dccb-rebrand/src/components/sections/about.me.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import dccb_about_picture from '../../assets/dccb_about_me_foto.png'
 
-/** DCCB-rebrand - Portfolio version 3.03 - Features: 
+/** DCCB-rebrand - Portfolio version 3.04 - Features: 
  * 
- *    --> Building AboutMeDCCB
+ *    --> adding 'dccb_about_picture' picture
  * 
  * Note: 'TailwindTest' is a component to test 
  * tailwind 
@@ -17,6 +18,21 @@ const AboutMeDCCB = () => {
                     04 // THE_ARCHITECT
                 </h2>
                 <div className="h-[1px] flex-1 bg-arch-gray/10 dark:bg-white/5"></div>
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
+
+                {/**LADO IZQUIERDO: Imagen con Estetica Tecnica (4 columnas) */}
+                <div className="lg:col-span-5 relative group">
+                    <div className="relative z-10 overflow-hidden rounded-sm grayscale hover:grayscale-0 transition-all duration-700 border border-slate-200 dark:border-white/10">
+                        {/* Aquí irá la foto de ella. Por ahora un placeholder con el aspect ratio de retrato */}
+                        <img 
+                            src={dccb_about_picture} 
+                            alt="Architect Portrait" 
+                            className="w-full h-auto object-cover"
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     )
